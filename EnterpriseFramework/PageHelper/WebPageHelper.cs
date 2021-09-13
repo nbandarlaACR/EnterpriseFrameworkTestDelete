2cd339229118562e07599872f9ca7057cc36eff0
Prashanth Sannayala <Prashanth.Sannayala@lowellgroup.co.uk>
2021-09-13 19:00:13 +0100

    Add project files.

---
+++ EnterpriseFramework/PageHelper/WebPageHelper.cs
@@ -0,0 +1,23 @@
+﻿using LFL.Automation.Framework.Actions;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LFL.Automation.Framework.PageHelper
+{
+    public static class WebPageHelper
+    {
+        private static readonly By acceptCookies = By.XPath("//div[@id='cookieConsent']/descendant::button");
+
+        public static void ClickAcceptCookiesIfPresent(IWebDriver driver)
+        {
+            bool cookies = WebActions.WaitForItem(driver, acceptCookies, 5);
+            if (cookies)
+            {
+                IWebElement element = driver.FindElement(acceptCookies);
+                element.Click();
+            }
+        }
+    }
+}
